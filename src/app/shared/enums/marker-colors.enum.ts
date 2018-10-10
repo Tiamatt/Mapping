c37e71a1c149de9b97049fe43cbf515f257c8a71
tiamatt <samira.yusifova@gmail.com>
2018-10-10 01:34:49 -0400

    Main page - design and style multiple components

--- src/app/shared/enums/marker-colors.enum.ts
+++ src/app/shared/enums/marker-colors.enum.ts
@@ -1,6 +1,6 @@
 export enum MarkerColorsEnum {
-    Northeast = "red",
-    Northwest = "blue",
-    Southeast = "green",
-    Southwest = "yellow",
+    Northeast = "#7A1414", // red
+    Northwest = "#355C7D", // blue
+    Southeast = "#00743F", // violet
+    Southwest = "#F26B38", // orange
 }

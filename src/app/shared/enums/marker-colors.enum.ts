fa46a15d8d9bfd9b661f6039207dcad887acea1d
tiamatt <samira.yusifova@gmail.com>
2018-10-10 15:23:18 -0400

    Main page - change marker colors

--- src/app/shared/enums/marker-colors.enum.ts
+++ src/app/shared/enums/marker-colors.enum.ts
@@ -1,6 +1,6 @@
 export enum MarkerColorsEnum {
-    Northeast = "#7A1414", // red
+    Northeast = "#00743F", // green
     Northwest = "#355C7D", // blue
-    Southeast = "#00743F", // violet
-    Southwest = "#F26B38", // orange
+    Southeast = "#7A1414", // red
+    Southwest = "#F7BA21", // yellow
 }
